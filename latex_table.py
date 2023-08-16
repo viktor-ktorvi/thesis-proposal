262daeb3dcd4953a15932d9eb2bf84502173bee4
viktor-ktorvi <todosijevicviktor998@gmail.com>
2023-08-16 14:19:25 +0200

    add simple pytohn->latex example

---
+++ latex_table.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df = pd.DataFrame(dict(name=['Raphael', 'Donatello'],
+                       age=[26, 45],
+                       height=[181.23, 177.65]))
+table_text = df.to_latex(
+    index=False,
+    formatters={"name": str.upper},
+    float_format="{:.1f}".format,
+)
+
+with open("latex/table.tex", "w") as file:
+    file.write(table_text)
