4e9db3dceb81d1d8d76df06521d523215a428c42
viktor-ktorvi <todosijevicviktor998@gmail.com>
2023-08-24 22:37:31 +0200

    add node types masks table; ref doesn't work

--- thesis_proposal_document/introduction/main.py
+++ thesis_proposal_document/introduction/main.py
@@ -1,20 +1,28 @@
 import os
 
-from pylatex import Command, Subsection, NoEscape
+from pylatex import Command, Subsection, NoEscape, NewLine, Marker, Ref
+
+from utils.latex.node_type_masks_table import node_type_mask_table
 
 
 def main():
     current_directory = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
     introduction = Subsection("Introduction", numbering=False)
-    problem_formulation = Subsection("Problem formulation", numbering=False)
-    problem_formulation.append(Command("input", "introduction/problem_formulation"))
+    introduction.append(Command("input", "introduction/basic_problem_statement"))
+    introduction.append(Command("input", "introduction/motivation"))
+    introduction.append(Command("input", "introduction/mathematical_formulation"))
 
-    motivation = Subsection("Motivation", numbering=False)
-    motivation.append(Command("input", "introduction/motivation"))
+    introduction.append(NewLine())
+    introduction.append(NewLine())
 
-    introduction.append(problem_formulation)
-    introduction.append(motivation)
+    introduction.append(Command("input", "introduction/node_types_and_masks"))
+
+    introduction.append(NewLine())
+    introduction.append(NewLine())
+
+    introduction.append(node_type_mask_table(Marker("nodemasks", prefix="tab")))
+
     with open(os.path.join(current_directory, "main.tex"), "w") as f:
         f.write(introduction.dumps())
 
